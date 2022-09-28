08e8863953a58763694b570c069a99737e54f0ac
Lunycat <lunycat@mail.ru>
2022-09-28 13:38:43 +0300

    add new Variable_1_9

--- JavaUrok/src/JavaTasks/Stepic/Java_for_beginners/StaticAndFinal_1_8/ClassA.java
+++ JavaUrok/src/JavaTasks/Stepic/Java_for_beginners/StaticAndFinal_1_8/ClassA.java
@@ -3,7 +3,19 @@ package JavaTasks.Stepic.Java_for_beginners.StaticAndFinal_1_8;
 public class ClassA {
     static final float PI = 3.14f;
 
+    float areaCircle(int radius) {
+        return PI * (float) Math.pow(radius, 2);
+    }
 
+    static float circleLength(int radius) {
+        return 2 * PI * radius ;
+    }
+
+    void info(int radius) {
+        System.out.println("Radius = " + radius);
+        System.out.println("Area of the Circle = " + areaCircle(radius));
+        System.out.println("Circle length = " + circleLength(radius));
+    }
 
     static int multiply(int a, int b, int c) {
         return a*b*c;
---
+++ JavaUrok/src/JavaTasks/Stepic/Java_for_beginners/Variable_1_9/ClassA.java
@@ -0,0 +1,7 @@
+package JavaTasks.Stepic.Java_for_beginners.Variable_1_9;
+
+public class ClassA {
+    public static void main(String[] args) {
+
+    }
+}
