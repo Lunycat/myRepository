13823f5c43e98c2602159707b11ef1df08ca89ec
Lunycat <lunycat@mail.ru>
2022-11-05 23:59:52 +0300

    update lesson 10

--- JavaUrok/src/Stepic/AdvancedJava/P3_Collections/Lesson10_Deque/ArrayDequeEx.java
+++ JavaUrok/src/Stepic/AdvancedJava/P3_Collections/Lesson10_Deque/ArrayDequeEx.java
@@ -6,6 +6,16 @@ public class ArrayDequeEx {
     public static void main(String[] args) {
         ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
 
+        arrayDeque.addFirst(4);
+        arrayDeque.addLast(6);
+        System.out.println(arrayDeque);
+        arrayDeque.offerFirst(10);
+        arrayDeque.offerLast(16);
+        System.out.println(arrayDeque);
+        arrayDeque.removeFirst();
+        arrayDeque.pollLast();
+        System.out.println(arrayDeque);
+
 
     }
 }
