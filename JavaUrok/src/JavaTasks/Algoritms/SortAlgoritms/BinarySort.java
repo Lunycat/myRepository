d2261d0e44dbe1771cb179f2717f354cc0caea0a
Lunycat <lunycat@mail.ru>
2022-11-24 22:25:27 +0300

    add quick sort v2

--- JavaUrok/src/JavaTasks/Algoritms/SortAlgoritms/BinarySort.java
+++ JavaUrok/src/JavaTasks/Algoritms/SortAlgoritms/BinarySort.java
@@ -4,19 +4,23 @@ import java.util.Arrays;
 
 public class BinarySort {
     public static void main(String[] args) {
-        int[] array = {32, 40, 10, 8, 12, 33, 4342, 35};
-        System.out.println(Arrays.toString(binarySort(array, 0, array.length - 1)));
+        int[] array = {32, 40, 10, 8, 12, 33, 43, 35};
+        quickSort(array);
+        System.out.println(Arrays.toString(array));
     }
 
-    static int[] binarySort(int[] array, int leftBorder, int rightBorder) {
+    static void quickSort(int[] array) {
+        quickSort(array, 0, array.length - 1);
+    }
+
+    static void quickSort(int[] array, int leftBorder, int rightBorder) {
         int leftMarker = leftBorder;
         int rightMarker = rightBorder;
         int pivot = array[(leftMarker + rightMarker) / 2];
-
-        while (leftBorder < rightBorder) {
+        while (leftMarker < rightMarker) {
             while (array[leftMarker] < pivot)
                 leftMarker++;
-            while (array[rightMarker] > pivot)
+            while (array[rightMarker] > pivot )
                 rightMarker--;
             if (leftMarker <= rightMarker) {
                 if (leftMarker < rightMarker) {
@@ -28,12 +32,8 @@ public class BinarySort {
                 rightMarker--;
             }
         }
-        if (leftMarker < rightBorder)
-            binarySort(array, leftMarker, rightBorder);
-        if (leftBorder < rightMarker)
-            binarySort(array, leftBorder, rightMarker);
-
-
-        return array;
+        if (leftMarker < rightBorder) quickSort(array, leftMarker, rightBorder);
+        if (leftBorder < rightMarker) quickSort(array, leftBorder, rightMarker);
     }
 }
+
