92bf85e911c2b8076381f42a23740088648e7025
Lunycat <lunycat@mail.ru>
2022-11-25 23:55:42 +0300

    update lesson41

--- JavaUrok/src/Stepic/AdvancedJava/P7_Files/Lesson41_BuffersAndChannels/Ex1.java
+++ JavaUrok/src/Stepic/AdvancedJava/P7_Files/Lesson41_BuffersAndChannels/Ex1.java
@@ -1,6 +1,5 @@
 package Stepic.AdvancedJava.P7_Files.Lesson41_BuffersAndChannels;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
@@ -8,26 +7,33 @@ import java.nio.channels.FileChannel;
 
 public class Ex1 {
     public static void main(String[] args) throws InterruptedException {
-        try (RandomAccessFile file = new RandomAccessFile("C:\\Users\\Артём\\Desktop\\text.txt", "rw");
-             FileChannel channel = file.getChannel())
+        try (RandomAccessFile file1 =
+                     new RandomAccessFile("C:\\Users\\xolms\\OneDrive\\Рабочий стол\\Источник.txt", "rw");
+             RandomAccessFile file2 =
+                     new RandomAccessFile("C:\\Users\\xolms\\OneDrive\\Рабочий стол\\Потребитель.txt", "rw");
+             FileChannel channelRead = file1.getChannel();
+             FileChannel channelWrite = file2.getChannel())
         {
-            ByteBuffer buffer = ByteBuffer.allocate(47);
             StringBuilder stih = new StringBuilder();
-            int byteRead = channel.read(buffer);
-            while (byteRead > 0) {
-                Thread.sleep(300);
-                System.out.println("Read: " + (char)byteRead);
-                buffer.flip();
-                while (buffer.hasRemaining()) {
-                    stih.append(buffer.get());
-                    Thread.sleep(300);
-                    System.out.println(stih);
+            ByteBuffer bufferRead = ByteBuffer.allocate(20);
+            int countReadBuffer = 1;
+            while (countReadBuffer > 0) {
+                countReadBuffer = channelRead.read(bufferRead);
+                System.out.printf("Было прочитано %d байт\n", countReadBuffer);
+                bufferRead.flip();
+                while (bufferRead.hasRemaining()) {
+                    stih.append((char) bufferRead.get());
                 }
-                buffer.clear();
-                byteRead = channel.read(buffer);
+                bufferRead.clear();
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.out.println("----------------------------------------------------------------");
+            System.out.println(stih);
+
+            file2.seek(300); // Установить курсор на 300 пункте
+            String text = "\nАбоба боба абобка";
+            ByteBuffer bufferWrite = ByteBuffer.wrap(text.getBytes());
+            channelWrite.write(bufferWrite);
         }
+        catch (IOException e) {throw new RuntimeException(e);}
     }
 }
